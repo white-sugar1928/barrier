263a9d1889e766e2352574ab2e7263ae16a82f5f
white-sugar1928 <87888047+white-sugar1928@users.noreply.github.com>
2024-12-28 01:00:00 +0900

    Update OSXKeyState.cpp
    for JP mac/win kana/eisuu

--- src/lib/platform/OSXKeyState.cpp
+++ src/lib/platform/OSXKeyState.cpp
@@ -128,11 +128,11 @@ static const KeyEntry    s_controlKeys[] = {
     { kKeyBrightnessDown, s_brightnessDown },
 
     // JIS keyboards only
-    { kKeyEisuToggle, kVK_JIS_Eisu },
-    { kKeyKana, kVK_JIS_Kana },
-    { kKeyMuhenkan, s_int5VK },
-    { kKeyHenkan, s_int4VK },
-    { kKeyZenkaku, kVK_ANSI_Grave }
+    { kKeyMuhenkan, kVK_JIS_Eisu },
+    { kKeyHenkan, kVK_JIS_Kana }
+    //{ kKeyMuhenkan, s_int5VK },
+    //{ kKeyHenkan, s_int4VK },
+    //{ kKeyZenkaku, kVK_ANSI_Grave }
 };
 
 
